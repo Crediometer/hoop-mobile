d0879a43a016061755b8b3b500aa26835d81a778
Raji Rasheed Abolarinwa <31524566+RAJIABEFE1412@users.noreply.github.com>
2026-01-16 20:37:45 +0100

    made changes to the UI

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Mac on 11/01/2026.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
